cdd6c1639d982ea96cb762392388937beb31b7a7
travisbennett03 <tbennett1996@yahoo.com>
2023-05-30 11:39:37 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Travis"
+let lastName = "Bennett"
+var age = 27
 // Exercise 2
-
+let fullName = firstName + "" + lastName;
+console.log(fullName);
 // Exercise 3
+let city = "Saint Charles";
+let passTime = "building computers"
+let myStory = `Hi, My name is ${fullName}. I live in ${city}. I enjoy ${passTime} and coding!`;
+console.log(myStory);
